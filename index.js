4a8e58edb580c7ced8fc4dbba0434934abaf31f6
Isakulov Tohir <isakulovtohir902@gmail.com>
2022-07-29 18:18:31 +0500

    almost done

--- index.js
+++ index.js
@@ -25,21 +25,59 @@ let alphabet = [
   "x",
   "y",
   "z",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
-console.log(alphabet);
+
 function CodelandUsernameValidation(str) {
-  // code goes here
   let input = `${str}`.split("");
-  console.log(input.length);
-  for(let i = 0; i < alphabet.length; i++) {
-    if(input[0] === alphabet[i]) {
-      console.log('nicee')
+  if(input.length >= 4 && input.length <= 25) {
+    let lastEl = input[input.length -1]
+    if(lastEl !== '_') {
+      if(!input.includes('!')){
+        console.log('ok')
+        for (let i = 0; i < alphabet.length; i++) {
+          if (input[0] === alphabet[i]) {
+            console.log("starts with " + input[0]);
+          }
+        } 
+      } else {
+        console.log('yoq')
+      }
+      
+    } else {
+      console.log('mumkinmas')
     }
+  } else {
+    console.log('not enough')
   }
+ 
   return input;
 }
 
-// keep this function call here
 
-
-console.log(CodelandUsernameValidation("fa_"));
+console.log(CodelandUsernameValidation("fa11w"));
